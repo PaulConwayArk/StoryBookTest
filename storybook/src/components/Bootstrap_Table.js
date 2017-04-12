201bd7d2f9f2dd94cf10108cfbdaf5a5dc22b08e
PaulConwayArk <paul.conway@ark-energy.eu>
2017-04-12 10:28:26 +0100

    [StoryBook] Remove add row

--- storybook/src/components/Bootstrap_Table.js
+++ storybook/src/components/Bootstrap_Table.js
@@ -29,8 +29,7 @@ const propTypes = {
   paginationPosition: PropTypes.string,
   search : PropTypes.bool,
   multiColumnSearch : PropTypes.bool,
-  exportCSV : PropTypes.bool,
-  insertRow : PropTypes.bool
+  exportCSV : PropTypes.bool
 };
 
 const defaultProps = {
@@ -61,8 +60,7 @@ const defaultProps = {
   sizePerPageList: null,
   search: false,
   multiColumnSearch: false,
-  exportCSV: false,
-  insertRow: false
+  exportCSV: false
 };
 
 function addProducts(quantity) {
@@ -98,28 +96,11 @@ function dateFormatter(cell, row) {
   return `${('0' + cell.getDate()).slice(-2)}/${('0' + (cell.getMonth() + 1)).slice(-2)}/${cell.getFullYear()}`;
 }
 
-
-function afterSearch(searchText, result) {
-  console.log('Your search text is ' + searchText);
-  console.log('Result is:');
-  for (let i = 0; i < result.length; i++) {
-    console.log('Fruit: ' + result[i].id + ', ' + result[i].name + ', ' + result[i].price);
-  }
-}
-
 class BootstrapTableView extends Component {
   csvFormatter(cell, row) {
     return `${row.id}: ${cell} USD`;
   }
 
-  onAfterInsertRow(row) {
-    let newRowStr = '';
-
-    for (const prop in row) {
-      newRowStr += prop + ': ' + row[prop] + ' \n';
-    }
-    alert('The new row is:\n ' + newRowStr);
-  }
 
   render() {
     const {
@@ -149,8 +130,7 @@ class BootstrapTableView extends Component {
       sizePerPageList,
       search,
       multiColumnSearch,
-      exportCSV,
-      insertRow
+      exportCSV
     } = this.props;
 
     this.options = {
@@ -166,8 +146,7 @@ class BootstrapTableView extends Component {
       lastPage: lastPage,
       paginationPosition: paginationPosition,
       page: page,
-      sizePerPageList: sizePerPageList,
-      afterInsertRow: this.onAfterInsertRow
+      sizePerPageList: sizePerPageList
     };
 
     this.filter = {
@@ -194,7 +173,6 @@ class BootstrapTableView extends Component {
         search = { search }
         multiColumnSearch = { multiColumnSearch }
         exportCSV = { exportCSV }
-        insertRow={ insertRow }
         striped hover condensed>
         <TableHeaderColumn
           dataField='id'
